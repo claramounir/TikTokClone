d7a3ef70e444147d278bd5cc6b9fb525150262c6
Clara Mounir Adly <claramounir555@gmail.com>
2024-03-30 21:03:37 +0200

    add files

---
+++ TikTokClone/App/TikTokCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokCloneApp.swift
+//  TikTokClone
+//
+//  Created by Clara Mounir Adly on 30/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TikTokCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
